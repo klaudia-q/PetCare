a4bf69f999691d771e4b7a6f0b5438692514ed68
Klaudia <kod_1313@vp.pl>
2024-11-24 18:17:13 +0100

    initial commit

---
+++ src/main/java/com/pracazaliczeniowa/pracazaliczeniowa/PracazaliczeniowaApplication.java
@@ -0,0 +1,13 @@
+package com.pracazaliczeniowa.pracazaliczeniowa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracazaliczeniowaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracazaliczeniowaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pracazaliczeniowa/pracazaliczeniowa/PracazaliczeniowaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pracazaliczeniowa.pracazaliczeniowa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracazaliczeniowaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
